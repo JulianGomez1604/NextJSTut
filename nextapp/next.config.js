78056043b15240ff34d321e85f2409bcb5dcbc32
JulianGomez1604 <123045080+JulianGomez1604@users.noreply.github.com>
2024-10-10 12:07:44 -0700

    Modified Config to allow external images made Logo link to Homepage

--- nextapp/next.config.js
+++ nextapp/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // Allows for external images
+    images:{
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname: "images.pexels.com"
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
--- nextapp/src/app/about/page.jsx
+++ nextapp/src/app/about/page.jsx
@@ -1,6 +1,13 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
     return (
-      <div>AboutPage</div>
+      <div>
+        <div className={styles.imgContainer}>
+          <Image src="https://images.pexels.com/photos/28818953/pexels-photo-28818953/free-photo-of-charming-cafe-exterior-with-vintage-signage.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width={500} height={500}/>
+        </div>
+      </div>
     )
   }
   
